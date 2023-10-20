25b69780143a4a6e59e2c3fc23df643fa4054027
robert <matthewrueter51@gmail.com>
2023-10-20 12:58:09 -0500

    add landing page rough draft.

--- property-pulse/frontend/src/app/page.tsx
+++ property-pulse/frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import Logo from "@/components/Logo";
 import Buyhouse from "@/components/landing/BuyhouseSvg";
 import ComingHome from "@/components/landing/Cominghome";
 import HomeSvg from "@/components/landing/HomeSvg";
+import Testimonial from "@/components/landing/Testimonial";
 
 
 export default function Home() {
@@ -15,7 +16,6 @@ export default function Home() {
         </div>
       </nav>
       <section className="flex items-center justify-between bg-lime-500 h-4/5">
-        
           <p className="p-5 h-1/2 w-1/2 text-darkblue text-sm md:text-4xl">
           Property Pulse streamlines all aspects of managing rental properties and fosters 
           a harmonious relationship between landlord and tenant.
@@ -23,16 +23,22 @@ export default function Home() {
         <HomeSvg />
       </section>
       
-      <section className="bg-green-800 h-4/5 md:h-1/2">
-        <h1>Information section</h1>
+      <section className="flex items-center bg-green-800 h-4/5 md:h-1/2 ">
         <Buyhouse />
+        <p className="p-5 h-full w-1/2 text-white text-sm md:text-4xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
       </section>
       
-      <section className="bg-lime-500 h-4/5 md:h-1/2">
-        <h1>Testimonials section</h1>
-        <ComingHome />
+      <section className="flex justify-center items-center bg-lime-500 min-h-4/5 md:min-h-1/2 ">
+        <div className="md:flex md:w-1/2 w-full h-1/2">
+          <Testimonial />
+          <Testimonial />
+          <Testimonial />
+        </div>
       </section>
-
     </main>
   )
 }
---
+++ property-pulse/frontend/src/components/landing/Testimonial.tsx
@@ -0,0 +1,14 @@
+
+const Testimonial = () =>{
+	return(
+		<div className="rounded-xl bg-slate-200 m-3">
+			<p className="p-3">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+			dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+			ex ea commodo consequat.
+			</p>
+			<p className="text-blue-800 p-1">By so and so</p>
+		</div>
+	)
+}
+export default Testimonial
