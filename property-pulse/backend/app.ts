ac796d29b03885742b493989e046f36b950d48c3
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-22 17:57:12 -0500

    Add database sessions

--- property-pulse/backend/app.ts
+++ property-pulse/backend/app.ts
@@ -1,22 +1,25 @@
 import express from 'express';
-require('express-async-errors')
+require('express-async-errors');
 import passport from 'passport';
 import session from 'express-session';
-const app = express()
-const cors = require('cors')
+import connectPgSimple from 'connect-pg-simple';
+const PgSession = connectPgSimple(session);
+const app = express();
+const cors = require('cors');
 import { PrismaClient } from '@prisma/client';
 import { Strategy as LocalStrategy } from './libs/strategy';
-import signupController from './app/auth/signup/signup-controller'
-import loginController from './app/auth/login/login-controller'
+import signupController from './app/auth/signup/signup-controller';
+import loginController from './app/auth/login/login-controller';
 
 const prisma = new PrismaClient();
 
-app.use(cors())
-// app.use(express.static('dist'))
-app.use(express.json())
+app.use(cors());
+// app.use(express.static('dist'));
+app.use(express.json());
 
 app.use(
   session({
+    store: new PgSession({}),
     secret: 'replaceme',
     resave: false,
     saveUninitialized: true,
@@ -27,7 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(LocalStrategy);
 
-app.use(signupController)
-app.use(loginController)
+app.use(signupController);
+app.use(loginController);
 
-module.exports = app
+module.exports = app;
