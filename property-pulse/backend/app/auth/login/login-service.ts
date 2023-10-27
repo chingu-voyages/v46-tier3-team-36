f473743ece0192642716a186473250a8e45f3938
mpark4656 <mpark4656@gmail.com>
2023-10-27 16:31:13 -0400

    Update authenticate to return specific msg from LocalStrategy

--- property-pulse/backend/app/auth/login/login-service.ts
+++ property-pulse/backend/app/auth/login/login-service.ts
@@ -1,9 +1,9 @@
 import passport from "passport";
 
 const loginUser = async (req, res, next) => {
-  passport.authenticate('local', (err, user) => {
-    if (err) return next(err);
-    if (!user) { return res.redirect('/login'); }
+  passport.authenticate('local', (err, user, info) => {
+	if (err) return next(err);
+    if (!user) return res.status(401).json({ msg: info.message });
 
     req.logIn(user, function(err) {
       if (err) return next(err);
