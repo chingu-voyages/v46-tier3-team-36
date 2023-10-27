7bd8159231c463f389a0d4275041b0bd7d8c7acb
robert <matthewrueter51@gmail.com>
2023-10-27 16:55:54 -0500

    clean up layout and styles of IssuesForm.

--- property-pulse/frontend/src/components/IssuesForm.tsx
+++ property-pulse/frontend/src/components/IssuesForm.tsx
@@ -1,29 +1,34 @@
 
-const formStyles = "flex flex-col justify-between absolute h-3/4 m-10 p-2 border-2 rounded-xl shadow-2xl shadow-black border-slate-400 bg-slate-200"
+const formStyles = "flex flex-col justify-around absolute md:w-1/3 h-3/4 m-10 p-2 border-2 rounded-xl shadow-xl shadow-slate-600 border-slate-400 bg-slate-100"
 const formSection ="flex flex-col"
-const inputStyles = "border-2 border-green-800";
-const btnInputStyles ="mb-4 w-full p-2 bg-green-800 hover:bg-green-600 text-white rounded";
+const inputStyles = "border-2 border-green-800 focus:outline-none focus-visible:border-green-600 rounded-xl p-2";
+const btnInputStyles ="mb-4 p-3 bg-green-800 hover:bg-green-600 text-white rounded";
+const textAreaStyles = "border-2 border-green-800 focus:outline-none focus-visible:border-green-600 rounded-xl p-2"
+const selectStyles ="flex flex-col font-medium mt-4 rounded-lg bg-green-800  text-white p-2";
 
+
+/* IssuesForm for tenant view allows tenants to create a new issue to send to PMs
+Tenant adds <Title> <type> & <Details> manually.
+<created by> <timestamp> <edited timestamp> etc... will be added to formdata once "submit btn" is clicked. 
+See schema for what keys:value types Issues should have.
+*/
 const IssuesForm = () => {
 	return(
-			<form className={formStyles}>
-				<section>
-					<div className={formSection}>
-						<label>Title</label>
-						<input className={inputStyles} type = "text" />
-					</div>
-					<div className={formSection}>
-						<label>Choose Type</label>
-						<select className="flex flex-col font-medium mt-4 rounded-lg bg-green-900 text-white" >
-							<option>Inquiry</option>
-							<option>Report</option>
-							<option>Request</option>
-						</select>
-					</div>
-				</section>
+			<form className={formStyles}>			
 				<div className={formSection}>
-					<label>Details</label>
-					<textarea/>
+					<label>Title</label>
+					<input placeholder="e.g.Broken window" className={inputStyles} type = "text" />
+				</div>
+				<div className={formSection}>
+					<label>Choose Type</label>
+					<select className={selectStyles}>
+						<option>Inquiry</option>
+						<option>Report</option>
+						<option>Request</option>
+					</select>
+				</div>			
+				<div className={formSection}>
+					<textarea rows={10} placeholder="details"className={textAreaStyles} />
 				</div>
 				<input className={btnInputStyles} type = "submit" />
 			</form>
