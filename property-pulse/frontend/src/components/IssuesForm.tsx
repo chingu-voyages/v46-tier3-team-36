4f309be0ee329c22401853a6e92a63f00754f405
robert <matthewrueter51@gmail.com>
2023-10-29 14:25:54 -0500

    add error handling to form submission.

--- property-pulse/frontend/src/components/IssuesForm.tsx
+++ property-pulse/frontend/src/components/IssuesForm.tsx
@@ -1,4 +1,6 @@
 'use client';
+import {useState} from 'react';
+import ErrorDisplay from "./ErrorDisplay";
 
 const formStyles = "flex flex-col justify-around absolute md:w-1/3 h-3/4 m-10 p-2 border-2 rounded-xl shadow-xl shadow-slate-600 border-slate-400 bg-slate-100"
 const formSection ="flex flex-col"
@@ -7,30 +9,39 @@ const btnInputStyles ="mb-4 p-3 bg-green-800 hover:bg-green-600 text-white round
 const textAreaStyles = "border-2 border-green-800 focus:outline-none focus-visible:border-green-600 rounded-xl p-2"
 const selectStyles ="flex flex-col font-medium mt-4 rounded-lg bg-green-800  text-white p-2";
 
-
+/*form tenants use to create an issue.
+-error component should probably be moved up to page level.
+ */
 const IssuesForm = () => {
+	const [error, setError ] = useState(false);
 
 	const handleSubmit = async (e: { preventDefault: () => void; target: any; }) =>{
 		e.preventDefault();
 		const form = e.target
 		const formData = new FormData(form);
 		const newIssue = Object.fromEntries(formData.entries());
-		
-		const response = await fetch('/api/admin/issues',{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(newIssue)
-		});
-		const data = await response.json();
-		console.log(data)// error - {msg: '\nInvalid `prisma.issue.create()` invocation in\n/ho…id value for argument `type`. Expected IssueType.'}
-
-		console.log(newIssue)
+		try{
+			const response = await fetch('/api/admin/issues',{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(newIssue)
+				//make an error body: undeclaredVar
+			});
+			//may use <data> in future for updating view(Redux) to avoid api call...
+			const data = await response.json();
+		}catch(error){
+			console.log(error)
+			setError(true)
+		}
 		form.reset();
 	}
+	if(error){
+		return <ErrorDisplay message="an error occured."/>
+	}else{
 
-	return(
+		return(
 			<form className={formStyles} onSubmit={handleSubmit}>			
 				<div className={formSection}>
 					<label>Title</label>
@@ -49,6 +60,7 @@ const IssuesForm = () => {
 				</div>
 				<input className={btnInputStyles} type = "submit" />
 			</form>
-	)
+		)
+	}
 };
 export default IssuesForm;
