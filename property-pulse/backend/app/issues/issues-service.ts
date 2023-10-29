2bab7a35887ba65f9206eedbec7a226369773acd
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-29 07:15:24 -0500

    add unitId to Issue

--- property-pulse/backend/app/issues/issues-service.ts
+++ property-pulse/backend/app/issues/issues-service.ts
@@ -3,12 +3,13 @@ import { PrismaClient, Issue } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const createIssue = async (user, issue: Issue) => {
-  const { type, title, description } = issue;
+  const { type, unitId, title, description } = issue;
 
   const createdIssue = await prisma.issue.create({
     data: {
       type,
       tenantId: user.id,
+      unitId,
       title,
       description
     }
