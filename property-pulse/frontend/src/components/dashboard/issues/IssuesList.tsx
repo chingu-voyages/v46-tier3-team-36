84cd77b347c3aa7b0bdf2e53f42964dbb74f70ad
robert <matthewrueter51@gmail.com>
2023-11-02 12:21:10 -0500

    move IssuesList css into separte file.

--- property-pulse/frontend/src/components/dashboard/issues/IssuesList.tsx
+++ property-pulse/frontend/src/components/dashboard/issues/IssuesList.tsx
@@ -1,5 +1,16 @@
 'use client'
 
+import {
+	listItem, 
+	listItemSection, 
+	headingStyles, 
+	issueTitle, 
+	date, 
+	buttonBox, 
+	editBtn, 
+	deleteBtn
+} from '@/lib/issuesListStyles';
+
 type Issue ={
 	id: number;
 	status: string;
@@ -12,16 +23,6 @@ type Issue ={
 type IssuesListProps = {
 	issues: Issue[];
 };
-//STYLES : move these to lib folder
-const listItem = "flex md:flex-row flex-col justify-between shadow-md hover:bg-green-50";
-const listItemSection ="flex flex-col gap-5 p-10 items-center w-full md:justify-between xl:flex-row";
-const headingStyles = "text-blue-600 text-xl "
-const issueTitle = "text-blue-600";
-const date ="text-slate-500 text-sm"
-const buttonBox = "flex flex-row gap-3 justify-end md:w-full"
-const editBtn = "py-1 px-3 rounded-full text-black bg-green-200 hover:bg-green-400 hover:text-white";
-const deleteBtn = "py-1 px-3 rounded-full text-white bg-red-300 hover:bg-red-700 hover:text-black";
-
 
 const IssuesList:React.FC<IssuesListProps> = ({issues}) => {
 	/* The 'issueType' names don't correspond to what user selects/sees e.g. 'maintenceRequest vs 'Request'
---
+++ property-pulse/frontend/src/lib/issuesListStyles.ts
@@ -0,0 +1,10 @@
+const listItem = "flex md:flex-row flex-col justify-between shadow-md hover:bg-green-50";
+const listItemSection ="flex flex-col gap-5 p-10 items-center w-full md:justify-between xl:flex-row";
+const headingStyles = "text-blue-600 text-xl "
+const issueTitle = "text-blue-600";
+const date ="text-slate-500 text-sm"
+const buttonBox = "flex flex-row gap-3 justify-end md:w-full"
+const editBtn = "py-1 px-3 rounded-full text-black bg-green-200 hover:bg-green-400 hover:text-white";
+const deleteBtn = "py-1 px-3 rounded-full text-white bg-red-300 hover:bg-red-700 hover:text-black";
+
+export {listItem, listItemSection, headingStyles, issueTitle, date, buttonBox, editBtn, deleteBtn}
