440241ea18db02f0075c641a1d45feff8be6b17c
robert <matthewrueter51@gmail.com>
2023-11-05 12:05:58 -0600

    add utility function to clean up updated issue after edit.

---
+++ property-pulse/frontend/src/utils/utilityFunctions.ts
@@ -0,0 +1,18 @@
+// getUpdatedObject takes an object with an arbitrary number of keys with any value type.
+// it returns an object containing only values which aren't equal to <"">
+// * in uncontrolled 'edit' forms this "parses" the object and avoids passing empty vaules to the update.
+const getUpdatedObject = (edit:Record<string, any>): Record<string, any> => {
+	const keys = Object.keys(edit);
+	let updatedObject:Record<string, any> = {};
+	
+	for(let i=0; i<=keys.length-1; i++){
+		const key = keys[i];
+		const value = edit[keys[i]];
+		if(edit[keys[i]] !== ""){
+			updatedObject[keys[i]] = edit[keys[i]];
+		}
+	}
+
+	return updatedObject;
+}
+export { getUpdatedObject };
